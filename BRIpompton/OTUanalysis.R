9cca5ac77635dbd461bd4f693b9bec7f759250d1
DevonO <do@DevonOs-MacBook-Air.local>
2018-03-13 10:09:52 -0400

    added OTUanalysis.R scripts for BRIpompton and Perlut projects

---
+++ BRIpompton/OTUanalysis.R
@@ -0,0 +1,16 @@
+## purpose: amptk OTU analysis
+## written: 28-jan-2018
+## edited: 13-mar-2018
+## author: devon orourke
+
+# ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ #
+               ######     Part 1 - data wrangling     ######
+# ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ #
+
+## install packages (run once) - commented out by default
+# install.packages('data.table')
+# install.packages('reshape2')
+# install.packages('tidyr')
+# install.packages('ggplot2')
+
+## load libraries:
